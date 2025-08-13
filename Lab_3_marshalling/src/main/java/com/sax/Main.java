d4112c242530dc395e87fc0933c24e197d3d53bb
MarinaCheSte <77856357+MarinaCheSte@users.noreply.github.com>
2025-08-13 22:54:24 +0300

    Add files via upload

---
+++ Lab_3_marshalling/src/main/java/com/sax/Main.java
@@ -0,0 +1,29 @@
+package com.sax;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String path = new File("E:\\Переподготовка\\Java_applications\\Projects\\Lab_3_marshalling\\" +
+                "src\\main\\java\\com\\books.xml").getAbsolutePath();
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            BookXMLHandler handler = new BookXMLHandler();
+            parser.parse(new File(path), handler);
+        } catch (IOException x) {
+            System.err.println(x);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
